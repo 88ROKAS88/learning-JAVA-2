f881ec324f25618572cc9e644d44c4cb798d4ec8
88ROKAS88 <rokas8misevicius@gmail.com>
2023-05-16 15:51:15 +0300

    Snake - key listener
    implements KeyListener

--- Projects/Snake/src/Snake.java
+++ Projects/Snake/src/Snake.java
@@ -1,10 +1,13 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Snake extends JFrame {
+public class Snake extends JFrame implements KeyListener {
 	
 	static JPanel grid[] = new JPanel[101];
 	
@@ -17,6 +20,7 @@ public class Snake extends JFrame {
 		panel.setLayout(new GridLayout(10, 10,1,1)); // layout manager (rows,columns,horizontal margin,vertical margin)
 		panel.setPreferredSize(new Dimension(260, 260));
 		panel.setBackground(Color.white);
+		this.addKeyListener(this);
 
 		for (int i = 1; i < 101; i++) {
 			grid[i] = new JPanel();
@@ -28,5 +32,22 @@ public class Snake extends JFrame {
 		this.pack();
 		this.setResizable(false);
 		this.setVisible(true);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+//		System.out.println("You released key char: " + e.getKeyChar());
+//		System.out.println("You released key code: " + e.getKeyCode());		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("You released key code: " + e.getKeyCode());		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
