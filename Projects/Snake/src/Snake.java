645d9ed414bbd0f5d7a807f1cc24d60dcb5d800c
88ROKAS88 <rokas8misevicius@gmail.com>
2023-05-17 09:41:52 +0300

    Snake - movement
    new TimerTask()

--- Projects/Snake/src/Snake.java
+++ Projects/Snake/src/Snake.java
@@ -3,21 +3,27 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Snake extends JFrame implements KeyListener {
-	
+
+	static int snakeBody = 45;
+	static int direction = 0;
+
 	static JPanel grid[] = new JPanel[101];
-	
+
 	Snake() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Snake");
 
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(10, 10,1,1)); // layout manager (rows,columns,horizontal margin,vertical margin)
+		panel.setLayout(new GridLayout(10, 10, 1, 1)); // layout manager (rows,columns,horizontal margin,vertical
+														// margin)
 		panel.setPreferredSize(new Dimension(260, 260));
 		panel.setBackground(Color.white);
 		this.addKeyListener(this);
@@ -27,11 +33,59 @@ public class Snake extends JFrame implements KeyListener {
 			grid[i].setBackground(Color.black);
 			panel.add(grid[i]);
 		}
+		grid[snakeBody].setBackground(Color.red);
 		this.add(panel);
 
 		this.pack();
 		this.setResizable(false);
 		this.setVisible(true);
+
+		// Timer Task
+		Timer timer = new Timer();
+
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("Tic ! " + snakeBody);
+				int movement = 0;
+				if (direction == 37) {
+					movement = -1;
+					if (snakeBody % 10 == 1) {
+						movement += 10;
+					}
+				}
+				if (direction == 39) {
+					movement = 1;
+					if (snakeBody % 10 == 0) {
+						movement += -10;
+					}
+				}
+				if (direction == 38) {
+					movement = -10;
+					if (snakeBody <= 10) {
+						movement += 100;
+					}
+				}
+				if (direction == 40) {
+					movement = 10;
+					if (snakeBody > 90) {
+						movement += -100;
+					}
+				}
+				System.out.println("Tac ! " + (snakeBody + movement));
+				drawSnakeBody(movement);
+			}
+		};
+		timer.scheduleAtFixedRate(task, 0, 1000); // (task , time or delay of first instance, how often repeat)
+	}
+
+	static void drawSnakeBody(int movement) {
+		// remove old location
+		grid[snakeBody].setBackground(Color.black);
+		// calculate new location
+		snakeBody += movement;
+		// add new location
+		grid[snakeBody].setBackground(Color.red);
 	}
 
 	@Override
@@ -42,12 +96,15 @@ public class Snake extends JFrame implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println("You released key code: " + e.getKeyCode());		
+		System.out.println("You released key code: " + e.getKeyCode());
+		if (e.getKeyCode() == 37 || e.getKeyCode() == 39 || e.getKeyCode() == 38 || e.getKeyCode() == 40) {
+			direction = e.getKeyCode();
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
